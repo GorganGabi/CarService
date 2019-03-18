ee8e59e3067c5e0ef450933ace9e0ed089280e62
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2019-03-18 23:00:14 +0200

    Fixed csproj conflicts due renaming of the folder

--- CarService.Service/Client/IClientService.cs
+++ CarService.Service/Client/IClientService.cs
@@ -6,6 +6,9 @@ namespace CarService.Service
 {
     public interface IClientService
     {
-        void CreateNew(SasiuDto auto);
+        void CreateNew(ClientDto auto);
+        ClientDto FindById(int autoId);
+        void Update(ClientDto autoDto);
+        void Delete(ClientDto autoDto);
     }
 }
--- CarService.Service/IDto.cs
+++ CarService.Service/IDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CarService.Service
 {
-    public class IComandaService
+    public interface IDto
     {
 
     }
